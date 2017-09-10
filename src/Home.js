f7f6096b8c3d7093fde0d8c717db726244a96ed2
nachum freedman <nochum09@gmail.com>
2017-09-10 08:24:39 +0300

    made a few ajustants to its responsive behavoior and added a contact page

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import './home.css';
 import HomePagePicture from './img/jerusalem.jpg';
 import HomeExamplesPicture from './img/icons.png';
 import HomeAboutPicture from './img/about.png';
+import TechscopeLogo from './img/techscopelogo.png';
 import HomeAboutReactPicture from './img/reactlogo.png';
 import HeaderMobile from './HeaderMobile';
 //Bob RosenSchein bobr@bobr.com answers.com
@@ -16,7 +17,7 @@ class Home extends Component  {
   componentDidMount(){
     window.addEventListener('scroll', (e)=>{
       const scrollY = window.scrollY;
-      const sectionHeight = document.body.scrollHeight / 3;
+      const sectionHeight = document.body.scrollHeight / 4;
       
       this.setState({ headerTop: Math.max( sectionHeight - 60 - scrollY, 0) });
     });
@@ -30,48 +31,53 @@ class Home extends Component  {
         <div className="page-section">
           <div className='techscope-header-container'>
             <div className='techscope-header'>
-              TechScope
+              <img src={TechscopeLogo} alt="" className='site-logo'/>
             </div>
-            <div className='techscope-body'>
-              Developers building Israel
-            </div>
+
             <img src={HomePagePicture} alt="" className='homepage-background'/>
           </div>
         </div>
         
         <div className='page-section third'>
           <div className='home-about-paper'>
+            <div className='home-about-paper-header'> About Us</div>
+            <div className='home-about-paper-body'>
+              We do our absolute best to give you not just the website you want, also a 'code-base' that will last for years, Because of that we only hire the best in tech and keep our team small to maximize quality.
+            </div>
+            <div className='home-about-paper-link'>Read more</div>
           </div>
           <div className='home-about-paper-white-1'>
             <img src={HomeAboutReactPicture}
                  alt=""
                  className='home-about-paper-white-1-img'/>
-            <h1 className='home-about-paper-white-1-header'>
-              We use React.JS
-            </h1>
-            <p className='home-about-paper-white-1-body'>Because we care about the product</p>
+            <div className='home-about-paper-white-1-text'>
+              <h1 className='home-about-paper-white-1-header'>
+                We use React.JS
+              </h1>
+              <p className='home-about-paper-white-1-body'>Because we care about the product
+              </p>
+
+            </div>
           </div>
           <div className='home-about-paper-white-2'></div>
           <div className='home-about-paper-white-3'></div>
-          <div className='home-about-paper-header'> About Us</div>
-          <div className='home-about-paper-body'>
-            We do our absolute best to give you not just the website you want, also a 'code-base' that will last for years, Because of that we only hire the best in tech and keep our team small to maximize quality.
-          </div>
-          <div className='home-about-paper-link'>Read more</div>
+          
+
           <img src={HomeAboutPicture} alt="" className='home-about-picture' />
         </div>
         
-        <div className="page-section second">
-          <img src={HomeExamplesPicture} alt="" className='homepage-background-second'/>
-          <div className='secondpage-paper'>
+        <div className="page-section example">
+          <img src={HomeExamplesPicture} alt="" className='homepage-background-example'/>
+          <div className='examplepage-paper'>
             <h1>Examples</h1>
             <p>Building components to user specifications is not always easy but building components to ours... is</p>
             <div>Read more >></div>
           </div>
-          <div className='triangle-bottom'>
-          </div>
         </div>
-        
+
+        <div className='page-section contact'>
+          <h1 className='home-contact-header-'>Contact</h1>
+        </div>
       </div>
     );
   }
